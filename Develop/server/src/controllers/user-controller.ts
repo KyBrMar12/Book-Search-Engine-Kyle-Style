4c2a5f97a9a85736231aac8f1f2b1a7f75e36ac6
KyBrMar12 <KyBrMar@gmail.com>
2025-03-12 10:36:31 -0500

    Enables the user model to include the savedBooks field for graphql

--- Develop/server/src/controllers/user-controller.ts
+++ Develop/server/src/controllers/user-controller.ts
@@ -1,35 +1,34 @@
 import type { Request, Response } from 'express';
-// import user model
+// Import user model
 import User from '../models/User.js';
-// import sign token function from auth
+// Import sign token function
 import { signToken } from '../services/auth.js';
 
-// get a single user by either their id or their username
+// Get a single user by ID or username
 export const getSingleUser = async (req: Request, res: Response) => {
   const foundUser = await User.findOne({
     $or: [{ _id: req.user ? req.user._id : req.params.id }, { username: req.params.username }],
   });
 
   if (!foundUser) {
-    return res.status(400).json({ message: 'Cannot find a user with this id!' });
+    return res.status(400).json({ message: 'Cannot find a user with this ID!' });
   }
 
   return res.json(foundUser);
 };
 
-// create a user, sign a token, and send it back (to client/src/components/SignUpForm.js)
+// Create a user, sign a token, and send it back
 export const createUser = async (req: Request, res: Response) => {
   const user = await User.create(req.body);
 
   if (!user) {
     return res.status(400).json({ message: 'Something is wrong!' });
   }
-  const token = signToken(user.username, user.password, user._id);
+  const token = signToken(user.username, user.email, String(user._id)); // Ensure _id is a string
   return res.json({ token, user });
 };
 
-// login a user, sign a token, and send it back (to client/src/components/LoginForm.js)
-// {body} is destructured req.body
+// Login a user, sign a token, and send it back
 export const login = async (req: Request, res: Response) => {
   const user = await User.findOne({ $or: [{ username: req.body.username }, { email: req.body.email }] });
   if (!user) {
@@ -41,12 +40,11 @@ export const login = async (req: Request, res: Response) => {
   if (!correctPw) {
     return res.status(400).json({ message: 'Wrong password!' });
   }
-  const token = signToken(user.username, user.password, user._id);
+  const token = signToken(user.username, user.email, String(user._id)); // Ensure _id is a string
   return res.json({ token, user });
 };
 
-// save a book to a user's `savedBooks` field by adding it to the set (to prevent duplicates)
-// user comes from `req.user` created in the auth middleware function
+// Save a book to a user's `savedBooks` field
 export const saveBook = async (req: Request, res: Response) => {
   try {
     const updatedUser = await User.findOneAndUpdate(
@@ -61,7 +59,7 @@ export const saveBook = async (req: Request, res: Response) => {
   }
 };
 
-// remove a book from `savedBooks`
+// Remove a book from `savedBooks`
 export const deleteBook = async (req: Request, res: Response) => {
   const updatedUser = await User.findOneAndUpdate(
     { _id: req.user._id },
@@ -69,7 +67,7 @@ export const deleteBook = async (req: Request, res: Response) => {
     { new: true }
   );
   if (!updatedUser) {
-    return res.status(404).json({ message: "Couldn't find user with this id!" });
+    return res.status(404).json({ message: "Couldn't find user with this ID!" });
   }
   return res.json(updatedUser);
 };
